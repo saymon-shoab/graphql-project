d8bfa25b05d7b4159a281aa4d0a430b13f5c4a2b
saymon-shoab <mdsaymonshoab@gmail.com>
2022-08-07 13:08:29 +0600

    add first scheema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,33 @@
-const graphql = require('graphal')
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt } = require('graphql')
+const graphql = require('graphql')
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
 const userType = new GraphQLObjectType({
     name: "user",
     discription: 'documantation for user',
     fields: () => ({
-        id: {type: GraphQLID},
+        id: {type: GraphQLString},
         name: {type: GraphQLString},
         age: {type: GraphQLInt}
     })
+})
+
+//root query
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    description:'description',
+    fields:{
+        user:{
+            type: userType,
+            args: {id:{type: GraphQLString}},
+            resolve(parent, args){
+             //we resolve with data
+             // get and return data from a datasource
+            }
+        }
+    }
+});
+
+module.exports =  new GraphQLSchema({
+    query: RootQuery
 })
